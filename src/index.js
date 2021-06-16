e9966ac1c07426351378f336557134a4dfbf901d
Luigi Matias <36388765+igiboi@users.noreply.github.com>
2021-06-16 12:00:35 -0400

    added export default notes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./components/App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.
